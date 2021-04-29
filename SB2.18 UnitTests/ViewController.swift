fa5bf1946baa5d2298ff34ff85a5dd9d3841c8ef
Artem Akopian <artem7498@gmail.com>
2021-04-29 12:42:56 +1000

    Initial Commit

---
+++ SB2.18 UnitTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB2.18 UnitTests
+//
+//  Created by Артём on 4/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
